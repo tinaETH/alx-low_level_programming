e7b2eef7830b4f5e9dc4c497fe96957cde44ee50
Mikiyas Solomon <mikiyasgreen@gmail.com>
2023-02-16 12:35:53 -0800

    Printf .c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/* this user program is print exactly on the string */
+	int main(void)
+	{	
+		puts ("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+	}
