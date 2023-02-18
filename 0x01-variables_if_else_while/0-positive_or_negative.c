e86f62c56520cfb599c1b7f71ec9cbdf55e1e087
Mikiyas Solomon <mikiyasgreen@gmail.com>
2023-02-17 19:03:16 -0800

    Positive or Negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,18 +2,19 @@
 #include <time.h>
 #include <stdio.h>
 
-/** randome numbers to the screen then tells the user if it s 
- * positive, nagative o r zero.
- */
-
 int main(void)
+
+	/** randome numbers to the screen then tells the user if it s
+	 * positive, nagative o r zero.
+	 */
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX /2;
-	if (n>0)
+	if ( n > 0)
 		printf("%d is Positive\n", n);
-	else if (n==0)
+	else if (n == 0)
 		printf("%d is Zero\n", n);
 	else 
 		printf("%d is Negative\n", n);
