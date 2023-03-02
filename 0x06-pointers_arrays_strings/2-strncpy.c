1875577dc9a42bd888979b5d4c7944c7b610283c
Mikiyas Solomon <mikiyasgreen@gmail.com>
2023-03-02 13:30:49 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy -copy a string
+ * @dest:input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return : dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j:
+		j=0;
+	while (j < N && src[j] != "\0")
+	{
+		dest[j] = src[j];
+	}
+	while (j < N)
+	{
+		dest[j] = "\0";
+		j++;
+	}
+	return (dest);
+}
