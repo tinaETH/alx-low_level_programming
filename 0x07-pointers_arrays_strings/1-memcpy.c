cd52a23183dfce15738faadc8da183aed155aa62
Mikiyas Solomon <mikiyasgreen@gmail.com>
2023-03-23 03:33:19 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,22 +1,19 @@
 #include "main.h"
-
 /**
- * _memcpy - Copies @n bytes from the memory area pointed
- *           to by @src into that pointed to by @dest.
- * @dest: A pointer to the memory area to copy @src into.
- * @src: The source buffer to copy characters from.
- * @n: The number of bytes to copy from @src.
- *
- * Return: A pointer to the destination buffer @dest.
+ * _memcpy - fills memory with another buffer.
+ * @dest: source string
+ * @src: string for filling
+ * @n: lenght of buffer
+ * Return: new string.
  */
-void *_memcpy(void *dest, const void *src, size_t n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
+	unsigned int i = 0;
 
-	for (index = 0; index < n; index++)
-		destination[index] = source[index];
-
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
 	return (dest);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 /**
  * File: main.h
- * Auth: Brennan D Baraban
+ * Auth: Mikiyas Solomon
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x06-pointers_arrays_strings directory.
  */
