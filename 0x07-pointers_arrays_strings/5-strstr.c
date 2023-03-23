019732045cb546f33a57ee3e5789c91f7c5744a6
Mikiyas Solomon <mikiyasgreen@gmail.com>
2023-03-23 04:02:53 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,27 +1,5 @@
 #include "main.h"
 /**
- * _strstr - prints the consecutive caracters of s1 that are in s2.
- * @haystack: source string
- * @needle: searching string
- *
- * Return: new string.
- */
-char *_strstr(char *haystack, char *needle)
-{
-	while (*haystack)
-	{
-		if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
-		{
-			return (haystack);
-		}
-		else
-		{
-			haystack++;
-		}
-	}
-	return (0);
-}
-/**
  * coincidence - define if the string b is inside a.
  * @a: source string
  * @b: string to be searched
